97212421397323ff0b57e6735c14c4e1cd10bd1a
Nguyễn Thị Thu Huyền <117350370+huyen2941003@users.noreply.github.com>
2023-11-22 15:33:01 +0700

    Update Program.cs

--- Thread_de3/Program.cs
+++ Thread_de3/Program.cs
@@ -15,16 +15,16 @@ namespace Thread_de3
         {
             ThreadStart op1 = new ThreadStart(Thread1);
             ThreadStart op2 = new ThreadStart(Thread2);
-
+    
             Thread thread1 = new Thread(op1);
             Thread thread2 = new Thread(op2);
-
+    
             thread1.Start();
             thread2.Start();
-
+    
             thread1.Join();
             thread2.Join();
-
+    
             Console.ReadKey();
         }
         static void Thread1()
@@ -37,13 +37,11 @@ namespace Thread_de3
                     Console.Write("Nhap chuoi ban muon nhap: ");
                     input = Console.ReadLine();
                     n = input;
+                    if (n == "thoat")
+                    {
+                        break;
+                    }
                 }
-
-                if (n == "thoat")
-                {
-                    break;
-                }
-
                 Thread.Sleep(5);
             }
         }
@@ -51,20 +49,17 @@ namespace Thread_de3
         {
             while (true)
             {
-                string currentInput;
                 lock (lockObject)
                 {
-                    currentInput = n;
+                    if (n == "thoat")
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"So luong tu trong chuoi la:  {Word(n).ToString()}");
                 }
-
-                if (n == "thoat")
-                {
-                    break;
-                }
-                Console.WriteLine($"So luong tu trong chuoi la:  {Word(currentInput).ToString()}");
                 Thread.Sleep(10);
             }
-
+    
         }
         static int Word(string input)
         {
